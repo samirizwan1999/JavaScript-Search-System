6d1f94eb6a448802a43c238750d050aea9ec84c2
samirizwan1999 <muhammadsami6972@gmail.com>
2022-08-12 06:14:45 +0500

    Task Complete

---
+++ app.js
@@ -0,0 +1,298 @@
+var mobiles = {
+    data: [
+        {
+            productTitle: "Iphone X",
+            category: "Iphone",
+            colors: "silver, spacegray",
+            price: "1250",
+            img: "images/X.png",
+            capacity: "64GB" +" "+ "256GB",
+        },
+        {
+            productTitle: "Iphone XS",
+            category: "Iphone",
+            colors: "gold, spacegray, white",
+            price: "1250",
+            img: "images/XS.png",
+            capacity: "64GB" +" "+ "128GB" +" "+ "256GB",
+        },
+        {
+            productTitle: "Iphone XR",
+            category: "Iphone",
+            colors: "red, white, yellow, blue",
+            price: "1350",
+            img: "images/XR.png",
+            capacity: "64GB" +" "+ "128GB" +" "+ "256GB",
+        },
+        {
+            productTitle: "Iphone 11",
+            category: "Iphone",
+            colors: "red, white, yellow, blue",
+            price: "1350",
+            img: "images/11.png",
+            capacity: "64GB" +" "+ "128GB" +" "+ "256GB",
+        },
+        {
+            productTitle: "Iphone 11 pro",
+            category: "Iphone",
+            colors: "red, white, yellow, blue",
+            price: "1350",
+            img: "images/11 pro.png",
+            capacity: "64GB" +" "+ "128GB" +" "+ "256GB",
+        },
+        {
+            productTitle: "Iphone 11 pro max",
+            category: "Iphone",
+            colors: "red, white, yellow, blue",
+            price: "1350",
+            img: "images/11 pro max.png",
+            capacity: "64GB" +" "+ "128GB" +" "+ "256GB",
+        },
+        {
+            productTitle: "Iphone 12",
+            category: "Iphone",
+            colors: "red, white, yellow, blue",
+            price: "1350",
+            img: "images/12.png",
+            capacity: "64GB" +" "+ "128GB" +" "+ "256GB",
+        },
+        {
+            productTitle: "Iphone 12 pro",
+            category: "Iphone",
+            colors: "red, white, yellow, blue",
+            price: "1350",
+            img: "images/12 pro.png",
+            capacity: "64GB" +" "+ "128GB" +" "+ "256GB",
+        },
+        {
+            productTitle: "Iphone 12 pro max",
+            category: "Iphone",
+            colors: "red, white, yellow, blue",
+            price: "1350",
+            img: "images/12 pro max.png",
+            capacity: "64GB" +" "+ "128GB" +" "+ "256GB",
+        },
+        {
+            productTitle: "Iphone 13",
+            category: "Iphone",
+            colors: "red, white, yellow, blue",
+            price: "1350",
+            img: "images/13.png",
+            capacity: "64GB" +" "+ "128GB" +" "+ "256GB",
+        },
+        {
+            productTitle: "Iphone 13 pro",
+            category: "Iphone",
+            colors: "red, white, yellow, blue",
+            price: "1350",
+            img: "images/13 pro.png",
+            capacity: "64GB" +" "+ "128GB" +" "+ "256GB",
+        },
+        {
+            productTitle: "Iphone 13 pro max",
+            category: "Iphone",
+            colors: "red, white, yellow, blue",
+            price: "1350",
+            img: "images/13 pro max.jpg",
+            capacity: "64GB" +" "+ "128GB" +" "+ "256GB",
+        },
+        {
+            productTitle: "Samsung s20",
+            category: "Samsung",
+            colors: "red, white, yellow, blue",
+            price: "1350",
+            img: "images/s20.png",
+            capacity: "64GB" +" "+ "128GB" +" "+ "256GB",
+        },
+        {
+            productTitle: "Samsung s20 Plus",
+            category: "Samsung",
+            colors: "red, white, yellow, blue",
+            price: "1350",
+            img: "images/s20 plus.png",
+            capacity: "64GB" +" "+ "128GB" +" "+ "256GB",
+        },
+        {
+            productTitle: "Samsung s20 Ultra",
+            category: "Samsung",
+            colors: "red, white, yellow, blue",
+            price: "1350",
+            img: "images/s20 ultra.png",
+            capacity: "64GB" +" "+ "128GB" +" "+ "256GB",
+        },
+        {
+            productTitle: "Samsung s22",
+            category: "Samsung",
+            colors: "red, white, yellow, blue",
+            price: "1350",
+            img: "images/s22.png",
+            capacity: "64GB" +" "+ "128GB" +" "+ "256GB",
+        },
+        {
+            productTitle: "Google Pixel 3",
+            category: "Google",
+            colors: "red, white, yellow, blue",
+            price: "1350",
+            img: "images/Google-Pixel-3.png",
+            capacity: "64GB" +" "+ "128GB" +" "+ "256GB",
+        },
+        {
+            productTitle: "Google Pixel 4",
+            category: "Google",
+            colors: "red, white, yellow, blue",
+            price: "1350",
+            img: "images/Google-Pixel-4.png",
+            capacity: "64GB" +" "+ "128GB" +" "+ "256GB",
+        },
+        {
+            productTitle: "Google Pixel 5",
+            category: "Google",
+            colors: "red, white, yellow, blue",
+            price: "1350",
+            img: "images/Google-Pixel-5.png",
+            capacity: "64GB" +" "+ "128GB" +" "+ "256GB",
+        },
+        {
+            productTitle: "Google Pixel 6",
+            category: "Google",
+            colors: "red, white, yellow, blue",
+            price: "1350",
+            img: "images/Google-Pixel-6.png",
+            capacity: "64GB" +" "+ "128GB" +" "+ "256GB",
+        },
+        {
+            productTitle: "Redmi Note 9",
+            category: "Xiomi",
+            colors: "red, white, yellow, blue",
+            price: "1350",
+            img: "images/Redmi Note9.png",
+            capacity: "64GB" +" "+ "128GB" +" "+ "256GB",
+        },
+        {
+            productTitle: "Redmi Note 10",
+            category: "Xiomi",
+            colors: "red, white, yellow, blue",
+            price: "1350",
+            img: "images/Redmi Note10.png",
+            capacity: "64GB" +" "+ "128GB" +" "+ "256GB",
+        },
+        {
+            productTitle: "Redmi Note 11",
+            category: "Xiomi",
+            colors: "red, white, yellow, blue",
+            price: "1350",
+            img: "images/Redmi Note 11.png",
+            capacity: "64GB" +" "+ "128GB" +" "+ "256GB",
+        },
+        {
+            productTitle: "Redmi Note 11 SE",
+            category: "Xiomi",
+            colors: "red, white, yellow, blue",
+            price: "1350",
+            img: "images/Xiaomi-Redmi-Note-11-SE.png",
+            capacity: "64GB" +" "+ "128GB" +" "+ "256GB",
+        },
+        {
+            productTitle: "Redmi Note 12",
+            category: "Xiomi",
+            colors: "red, white, yellow, blue",
+            price: "1350",
+            img: "images/Xiaomi-12-Pro-Purple-1.png",
+            capacity: "64GB" +" "+ "128GB" +" "+ "256GB",
+        },
+    ],
+}
+
+
+for (var i of mobiles.data) {
+    var card = document.createElement("div");
+    card.classList.add("card", i.category, "hide")
+    var imageContainer = document.createElement("div");
+    imageContainer.classList.add("image-container");
+    var image = document.createElement("img");
+    image.setAttribute("src", i.img);
+    card.appendChild(imageContainer);
+    imageContainer.appendChild(image);
+    var container = document.createElement("div");
+    container.classList.add("container");
+    // for getting the product Title
+    var names = document.createElement("h3");
+    names.classList.add("product-name");
+    names.innerText = i.productTitle.toUpperCase();
+    container.appendChild(names);
+    // For getting Price
+    var price = document.createElement("h4");
+    price.classList.add("price");
+    price.innerText = "$" + i.price;
+    container.appendChild(price);
+    card.appendChild(container);
+    // For getting Capacity
+    var capacity = document.createElement("h5");
+    capacity.classList.add("capacity");
+    capacity.innerText = i.capacity;
+    container.appendChild(capacity);
+    // For Getting Colors
+    var colors = document.createElement("h5");
+    colors.classList.add("colors");
+    colors.innerText = i.colors;
+    container.appendChild(colors)
+
+    document.getElementById("products").appendChild(card);
+
+}
+
+function filterProduct(value) {
+    var buttons = document.querySelectorAll(".button-value");
+    buttons.forEach((button) => {
+        if (value.toUpperCase() === button.innerText.toUpperCase()) {
+            button.classList.add("active");
+        }
+        else {
+            button.classList.remove("active");
+        }
+    })
+    var elements = document.querySelectorAll(".card");
+    elements.forEach((element) => {
+        if (value == "all") {
+            element.classList.remove("hide");
+        }
+        else{
+            if(element.classList.contains(value)){
+                element.classList.remove("hide");
+            }
+            else{
+                element.classList.add("hide");
+            }
+        }
+    })
+}
+
+document.getElementById("search").addEventListener
+("click", () => {
+    var searchInput = document.getElementById("search-input").value;
+    var elements = document.querySelectorAll(".product-name");
+    var cards = document.querySelectorAll(".card");
+    var message =  "Product Not Found";
+    document.getElementById("message").innerHTML = message;
+    
+
+
+
+    elements.forEach((element, index) => {
+        if(element.innerText.includes(searchInput.toUpperCase())){
+            cards[index].classList.remove("hide");
+            document.getElementById("message").style.display = "none";
+            
+        }
+        else{
+            cards[index].classList.add("hide");
+            document.getElementById("message").style.display = "block";
+        }
+    })
+})
+
+
+window.onload = () => {
+    filterProduct("all");
+};
+// console.log(mobiles[key])
